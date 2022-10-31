1db1c0bc6aab84d475f0fd8df1b9e6558f3f109a
leoD-E1 <leodangeli11@gmail.com>
2022-10-30 21:54:03 -0300

    [update] info in Services, Footer

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -2,7 +2,7 @@ const About = () => {
 	return (
 		<div className='h-[100vh]'>
 			<div className='flex h-full'>
-				<div className='w-[50%] bg-leoda bg-fixed bg-no-repeat bg-fill bg-left'></div>
+				<div className='w-[50%] bg-leoda bg-cover bg-center'></div>
 				<div className='flex justify-center items-center w-[50%] text-white bg-dark-gray'>
 					<div className='p-10'>
 						<h4 className='text-2xl text-orange'>about me</h4>
